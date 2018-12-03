87167166f1227305a999e9f7dc0a719d8e43957d
Antony Chan <antony.chan@oocl.com>
2018-12-03 14:28:34 +0800

    [Antony]: finish practice 4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {number: 0};
+  updateNumber = () => {
+    this.setState({number: 1});
+  }
+
   render() {
     return (
       <div>
-        <button onClick={function(){alert('now you know how to handle onclick event')}}>
+        <button onClick={this.updateNumber}>
           Hello world!
         </button>
         <span>
-          number: 0
+          number: {this.state.number}
         </span>
       </div>
     );
